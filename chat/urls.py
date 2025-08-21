b663822ba91b904446ea0e05d0ff3340c365b996
sandeep6268 <sandeepsinghkhicchi@gmail.com>
2025-08-21 21:55:27 +0530

    Fix STATIC_ROOT configuration

--- chat/urls.py
+++ chat/urls.py
@@ -3,13 +3,13 @@ from . import views
 
 urlpatterns = [
     path('', views.chat_view, name='chat'),
-    path('chat/register/', views.register_view, name='register'),
-    path('chat/login/', views.login_view, name='login'),
-    path('chat/logout/', views.logout_view, name='logout'),
-    path('chat/room/<int:user_id>/', views.get_or_create_chatroom, name='chat_room'),
-    path('chat/send/<int:room_id>/', views.send_message, name='send_message'),
-    path('chat/messages/<int:room_id>/', views.get_messages, name='get_messages'),
-    path('chat/unread_counts/', views.get_unread_counts, name='unread_counts'),  # Add this line
-    path('chat/update_status/', views.update_user_status, name='update_status'),
-    path('chat/user_status/<int:user_id>/', views.get_user_status, name='user_status'),
+    path('register/', views.register_view, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('room/<int:user_id>/', views.get_or_create_chatroom, name='chat_room'),
+    path('send/<int:room_id>/', views.send_message, name='send_message'),
+    path('messages/<int:room_id>/', views.get_messages, name='get_messages'),
+    path('unread_counts/', views.get_unread_counts, name='unread_counts'),  # Add this line
+    path('update_status/', views.update_user_status, name='update_status'),
+    path('user_status/<int:user_id>/', views.get_user_status, name='user_status'),
 ]
